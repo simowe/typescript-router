afc872c03f81a358298df09808a9d6f53de1385b
Simen Owesen-Lein <simowe@attensi.com>
2024-05-24 16:14:43 +0200

    delete old src

--- src_old/RouteParams.ts
+++
@@ -1,9 +0,0 @@
-import { routeConfig } from "./routeConfig"
-import { ViewParams } from "./routeUtils"
-
-type Routes = (typeof routeConfig)["routes"][number]
-type Name = (typeof routeConfig)["routes"][number]["name"]
-
-export type RouteParams<N extends Name> = ViewParams<
-  Extract<Routes, { name: N }>["path"]
->
