cc12c49e9bbc6fffd367e898653eaa9ef8eafc86
Simen Owesen-Lein <simowe@attensi.com>
2024-05-24 16:12:38 +0200

    remove console log

--- dev/pages/ExplorePage.tsx
+++ dev/pages/ExplorePage.tsx
@@ -4,7 +4,6 @@ export const ExplorePage = ({
   searchQuery,
   setSearchQuery,
 }: PortalRouteParams<"explorePage">) => {
-  console.log({ searchQuery, setSearchQuery })
   return (
     <>
       Explore
